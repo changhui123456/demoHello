4d5acb973250f01e670547984697537f9a59e2a9
changhui123456 <273722919@qq.com>
2020-06-19 11:12:44 +0800

    测试创建线程

---
+++ src/main/java/test/threadPool/TestThreadPool.java
@@ -0,0 +1,17 @@
+package test.threadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class TestThreadPool {
+
+    public static ExecutorService newSingleThreadExecutor() {
+        /*return new FinalizableDelegatedExecutorService
+                (new ThreadPoolExecutor(1, 1,
+                        0L, TimeUnit.MILLISECONDS,
+                        new LinkedBlockingQueue<Runnable>()));*/
+        return null;
+    }
+}
